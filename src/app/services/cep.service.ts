6fbc571d1a7a7e81f0cf1272c7ecb60fa843d91a
airtonferreira <airton.felix@dce.ufpb.br>
2019-07-30 00:05:05 -0300

    buscar method

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -9,4 +9,11 @@ export class CepService {
   constructor(
     private http: HttpClient
   ) { }
+
+  buscar(cep: string){
+    this.http.get(`https://viacep.com.br/ws/${cep}/json/`).toPromise().then(response =>  {
+      console.log(response);
+    });
+  }
+
 }
