9d9bc599949e4273f8f0879835dc966726dd5205
airtonferreira <airton.felix@dce.ufpb.br>
2019-07-30 00:00:01 -0300

    Inject HttpCliente on cep service

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
