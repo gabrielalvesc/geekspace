7b7e4c5b67592029a112c7dceb9579c2f74b27d8
airtonferreira <airton.felix@dce.ufpb.br>
2019-09-20 22:28:55 -0300

    Login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,34 +5,45 @@ import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { Cart } from '../models/cart.model';
 import { Product } from '../models/product.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { GEEK_API } from './geek.api';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
+  private headers = new HttpHeaders({
+    'Access-Control-Allow-Origin': '*'
+  });
 
   constructor(
     private userService: UserService,
     private toastr: ToastrService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {
 
   }
 
   login(email: string, password: string) {
-    let user: User = this.userService.getByEmail(email);
-    if (user != null) {
-      if (user.password == password) {
-        localStorage.setItem('user', email)
-        this.router.navigate(['/dashboard'])
-      } else {
-        this.toastr.error('Acesso negado', 'Tente novamente!')
-      }
-    } else {
-      this.toastr.error('Usuário inválido', 'Tente novamente!')
-    }
+    // let user: User = this.userService.getByEmail(email);
+    // if (user != null) {
+    //   if (user.password == password) {
+    //     localStorage.setItem('user', email)
+    //     this.router.navigate(['/dashboard'])
+    //   } else {
+    //     this.toastr.error('Acesso negado', 'Tente novamente!')
+    //   }
+    // } else {
+    //   this.toastr.error('Usuário inválido', 'Tente novamente!')
+    // }
+
+    // tslint:disable-next-line: max-line-length
+    return this.http.post(`${GEEK_API}/login`, JSON.stringify({ email, password }), { headers: this.headers }).pipe(map((response: Response) => localStorage.setItem('data', JSON.stringify(response))));
+
   }
 
   getUser() {
