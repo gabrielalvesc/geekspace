055358cc88e278dec8146e6cd571bff192ff93fa
gabriel <gabriel.alves@dce.ufpb.br>
2019-09-22 03:10:49 -0300

    Adding product to the favorites

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -5,48 +5,48 @@ import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { GEEK_API } from './geek.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
 
-  // favorites: Product[] = []
+  favorites: GenericProduct[];
+  clientId: number;
 
   constructor(
     private authService: AuthService,
     private toastr: ToastrService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {
-    // let user:User = this.userService.getByEmail(this.authService.getUser())
-    // this.favorites = user.favorites
-   }
-
-  // getFavorites() {
-  //   return this.favorites;
-  // }
-
-  // addFavorite(product: Product) {
-  //   if(this.authService.isLoggedIn()) {
-  //     this.favorites.push(product);
-  //     this.toastr.info('Este produto foi adicionado na sua lista de favoritos', 'Adicionado aos favoritos', {
-  //       timeOut: 2000,
-  //       positionClass: 'toast-top-left',
-  //     });
-  //   } else {
-  //     this.router.navigate(['/conta'])
-  //   }
     
-    // let user:User = this.userService.getByEmail(this.authService.getUser())
-    // user.favorites = this.favorites
-  // }
+  }
 
-  // removeFavorite(id:number) {
-  //   this.favorites = this.favorites
-  //   .filter(todo => todo.idProduct !== id);
-  //   this.toastr.info("Produto removido da sua lista de favoritos", "Produto removido", {
-  //     timeOut: 2000,
-  //     positionClass: 'toast-top-left',
-  //   });
-  // }
+  getFavorites(clientId: number) {
+    return this.http.get<GenericProduct[]>(`${GEEK_API}/clients/${clientId}/favorites`)
+  }
+
+  addFavorite(productId: number) {
+    if (this.authService.isLoggedIn()) {
+      this.http.post(`${GEEK_API}/clients/${this.authService.getUser()}/favorites/new/${productId}`, null).subscribe(res => {
+        console.log(res);
+        this.getFavorites(this.authService.getUser());
+        this.toastr.info('Este produto foi adicionado na sua lista de favoritos', 'Adicionado aos favoritos', {
+          timeOut: 2000,
+          positionClass: 'toast-top-left',
+        });
+      })
+    } else {
+      this.router.navigate(['/conta'])
+    }
+  }
+
+  removeFavorite(id:number) {
+    this.http.delete(`${GEEK_API}/clients/${this.authService.getUser()}/favorites/${id}`).subscribe(res =>{
+      this.toastr.info('Produto foi removido da sua list de favoritos', 'Removido')
+    })
+  }
 }
